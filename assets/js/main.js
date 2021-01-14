d1d2003c69598ca00e89e1f76cc6da39fc6ea852
TranDangKhoa2213 <dkhoa2213@gmail.com>
2021-01-15 00:11:46 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link');
+const navMenu = document.getElementById('nav-menu');
+
+const linkAction = () => {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//scroll section active link
+
+const sections = document.querySelectorAll('section[id]');
+window.addEventListener('scroll',scrollActive);
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*='+sectionId+'').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*='+sectionId+'').classList.remove('active')
+        }
+    })
+}
+
+//change color header
+window.scroll = () => {
+    const nav = document.getElementById('header');
+    if (this.scroll >= 200) {
+        nav.classList.add('scroll-header')
+    }else{
+        nav.classList.remove('scroll-header')
+    }
+}
